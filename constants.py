a0c7f333ce00fd8610050c564a206fd5557be38d
Paul Hart <jphart@glentech.com>
2021-01-30 09:49:57 -0800

    Add vaccinations completed

--- constants.py
+++ constants.py
@@ -31,6 +31,8 @@ BC_REGIONAL_URL = 'http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_
 # Canada Vaccination stats
 # "date_vaccine_administered","province","cumulative_avaccine"
 CANADA_VACCINATION_ADMINSTERED = 'https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_administration_timeseries_prov.csv'
+# "province","date_vaccine_completed","cvaccine","cumulative_cvaccine"
+CANADA_VACCINATION_COMPLETED = 'https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_completion_timeseries_prov.csv'
 # "date_vaccine_distributed","province","cumulative_dvaccine"
 CANADA_VACCINATION_DISTRIBUTED = 'https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_distribution_timeseries_prov.csv'
 
--- src/pages/vaccinations.py
+++ src/pages/vaccinations.py
@@ -28,6 +28,12 @@ def write():
     dfAdmin = dfAdmin.sort_values(['date_vaccine_administered64'], ascending=[True])
     dfAdmin['cumulative_avaccine_mean'] = dfAdmin['cumulative_avaccine'].rolling(7).mean()
 
+    dfCompl = pd.read_csv(cn.CANADA_VACCINATION_COMPLETED)
+    dfCompl['date_vaccine_completed64']= pd.to_datetime(dfCompl['date_vaccine_completed'], format='%d-%m-%Y')
+    dfCompl['date_vaccine_completed'] = dfCompl['date_vaccine_completed64'].dt.strftime('%Y-%m-%d')
+    dfCompl = dfCompl.sort_values(['date_vaccine_completed64'], ascending=[True])
+    dfCompl['cumulative_cvaccine_mean'] = dfCompl['cumulative_cvaccine'].rolling(7).mean()
+
     dfDistr = pd.read_csv(cn.CANADA_VACCINATION_DISTRIBUTED)
     dfDistr['date_vaccine_distributed64']= pd.to_datetime(dfDistr['date_vaccine_distributed'], format='%d-%m-%Y')
     dfDistr['date_vaccine_distributed'] = dfDistr['date_vaccine_distributed64'].dt.strftime('%Y-%m-%d')
@@ -45,12 +51,12 @@ def write():
     st.title("Vaccinations by Day")
     cn.DATE_SPANS()
 
-    writeProvinceGraph(dfAdmin, dfDistr, 'BC')
-    writeProvinceGraph(dfAdmin, dfDistr, 'Alberta')
-    writeProvinceGraph(dfAdmin, dfDistr, 'Ontario')
-    writeProvinceGraph(dfAdmin, dfDistr, 'Quebec')
+    writeProvinceGraph(dfAdmin, dfDistr, dfCompl, 'BC')
+    writeProvinceGraph(dfAdmin, dfDistr, dfCompl, 'Alberta')
+    writeProvinceGraph(dfAdmin, dfDistr, dfCompl, 'Ontario')
+    writeProvinceGraph(dfAdmin, dfDistr, dfCompl, 'Quebec')
 
-def writeProvinceGraph(dfAdmin, dfDistr, province):
+def writeProvinceGraph(dfAdmin, dfDistr, dfCompl, province):
     dfAdmin = dfAdmin[dfAdmin['province'] == f'{province}']
     dfDistr = dfDistr[dfDistr['province'] == f'{province}']
 
@@ -69,7 +75,8 @@ def writeProvinceGraph(dfAdmin, dfDistr, province):
 
     plt.bar(dfDistr['date_vaccine_distributed'], dfDistr['cumulative_dvaccine'], label=f'{province} Vaccines Distributed')
     plt.bar(dfAdmin['date_vaccine_administered'], dfAdmin['cumulative_avaccine'], label=f'{province} Vaccines Administered')
-    plt.legend(['Distributed','Administered'])
+    plt.bar(dfCompl['date_vaccine_completed'], dfCompl['cumulative_cvaccine'], label=f'{province} Vaccines Completed')
+    plt.legend(['Distributed','Administered','Completed'])
     plt.grid(b=True, which='major')
     
     st.pyplot(fig1)
